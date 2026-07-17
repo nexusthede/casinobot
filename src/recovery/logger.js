73880f59192764e9ce4bed003a05b649fac7e8ae
nexusthede <ivanrrr76@gmail.com>
2026-07-17 02:01:40 -0700

    Update logger.js

--- src/recovery/logger.js
+++ src/recovery/logger.js
@@ -22,3 +22,83 @@ function timestamp() {
 }
 
 function write(level, message) {
+
+    const line = `[${timestamp()}] [${level}] ${message}\n`;
+
+    fs.appendFile(
+        getLogFile(),
+        line,
+        err => {
+            if (err) console.error(err);
+        }
+    );
+
+}
+
+function info(message) {
+    write("INFO", message);
+}
+
+function warn(message) {
+    write("WARN", message);
+}
+
+function error(message) {
+    write("ERROR", message);
+}
+
+function command(message, command) {
+
+    write(
+        "COMMAND",
+        `${message.author.tag} (${message.author.id}) used "${command}" in #${message.channel.name}`
+    );
+
+}
+
+function economy(user, action, amount) {
+
+    write(
+        "ECONOMY",
+        `${user.tag} (${user.id}) ${action} $${amount.toLocaleString()}`
+    );
+
+}
+
+function guildJoin(guild) {
+
+    write(
+        "GUILD",
+        `Joined ${guild.name} (${guild.id})`
+    );
+
+}
+
+function guildLeave(guild) {
+
+    write(
+        "GUILD",
+        `Left ${guild.name} (${guild.id})`
+    );
+
+}
+
+function backup(users) {
+
+    write(
+        "BACKUP",
+        `Created backup containing ${users.toLocaleString()} profiles`
+    );
+
+}
+
+module.exports = {
+    info,
+    warn,
+    error,
+    command,
+    economy,
+    guildJoin,
+    guildLeave,
+    backup
+};
