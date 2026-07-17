bcfbaf83a28421d2ff228efa7e8faa5391e079bf
nexusthede <ivanrrr76@gmail.com>
2026-07-17 05:59:20 -0700

    Create deposit.js

---
+++ src/commands/casino/deposit.js
@@ -0,0 +1,120 @@
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+const config = require("../../config");
+
+const {
+    getProfile,
+    saveProfile,
+    formatMoney
+} = require("../../utils/economy");
+
+
+module.exports = {
+
+    name: "deposit",
+
+    aliases: [
+        "dep"
+    ],
+
+
+    async execute(
+        client,
+        message,
+        args
+    ) {
+
+
+        const profile =
+            await getProfile(
+                message.guild.id,
+                message.author.id
+            );
+
+
+        let amount =
+            args[0]
+                ? Number(args[0])
+                : profile.wallet;
+
+
+
+        if (
+            !amount ||
+            amount <= 0 ||
+            isNaN(amount)
+        ) {
+
+            return message.reply(
+                "You don't have any money to deposit."
+            );
+
+        }
+
+
+
+        if (
+            amount > profile.wallet
+        ) {
+
+            return message.reply(
+                "You don't have enough money in your wallet."
+            );
+
+        }
+
+
+
+        profile.wallet -= amount;
+
+        profile.bank += amount;
+
+
+
+        await saveProfile(
+            profile
+        );
+
+
+
+        const embed =
+            new EmbedBuilder()
+
+            .setColor(config.embedColor)
+
+            .setTitle("Deposit")
+
+            .setDescription(
+`You deposited **$${formatMoney(amount)}** into your bank.
+
+**Wallet**
+\`$${formatMoney(profile.wallet)}\`
+
+**Bank**
+\`$${formatMoney(profile.bank)}\``
+            )
+
+            .setFooter({
+                text:
+                `Deposited by ${message.author.username}`,
+                iconURL: message.author.displayAvatarURL({
+                    dynamic: true
+                })
+            })
+
+            .setTimestamp();
+
+
+
+        return message.reply({
+            embeds: [
+                embed
+            ]
+        });
+
+
+    }
+
+};
